c37b2307870d1e76246b9d6bbfb8c5bfc7c68737
Robert <rwallhead@gmail.com>
2021-05-23 13:07:51 +1000

    Working reading of MSP!

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Using a BetaFlight-equipped flight controller to give the orientation.
 
 ### HappyModel SP Racing F3 EVO Brushed Micro FC
 
- * Running BetaFlight 3.1.7
+ * Running BetaFlight 3.0.1 without any configuration.
---
+++ arduino-client/arduino-client.ino
@@ -0,0 +1,83 @@
+/* 
+ *  Based on: https://gist.github.com/bonsi/9298780eefc6a0b1057f19779a901001
+ *  
+ *  Board: Arduino Nano
+  */
+#include <SoftwareSerial.h>
+
+#define MSP_ATTITUDE 108
+
+SoftwareSerial mspSerial(3, 2); // RX TX
+
+void setup() {
+    mspSerial.begin(9600);
+    Serial.begin(115200);
+    Serial.println("setup end");
+}
+
+void loop() {
+    uint8_t datad = 0;
+    uint8_t *data = &datad;
+
+    sendMSP(MSP_ATTITUDE, data, 0);
+    readData();
+}
+
+void sendMSP(uint8_t cmd, uint8_t *data, uint8_t n_bytes) {
+
+    uint8_t checksum = 0;
+
+    mspSerial.write((byte *) "$M<", 3);
+    mspSerial.write(n_bytes);
+    checksum ^= n_bytes;
+
+    mspSerial.write(cmd);
+    checksum ^= cmd;
+
+    mspSerial.write(checksum);
+}
+
+void readData() {
+    delay(100);
+
+    byte count = 0;
+
+    int16_t roll;
+    int16_t pitch;
+    int16_t yaw;
+
+    while (mspSerial.available()) {
+        count += 1;
+        byte c = mspSerial.read();
+        switch (count) {
+            case 6:
+                roll = c;
+                break;
+            case 7:
+                roll <<= 8;
+                roll += c;
+                roll = (roll & 0xFF00) >> 8 | (roll & 0x00FF) << 8; // Reverse the order of bytes
+                break;
+            case 8:
+                pitch += c;
+                break;
+            case 9:
+                pitch <<= 8;
+                pitch += c;
+                pitch = (pitch & 0xFF00) >> 8 | (pitch & 0x00FF) << 8; // Reverse the order of bytes
+                break;
+            case 10:
+                yaw += c;
+                break;
+            case 11:
+                yaw <<= 8;
+                yaw += c;
+                yaw = (yaw & 0xFF00) >> 8 | (yaw & 0x00FF) << 8; // Reverse the order of bytes
+                break;
+        }
+    }
+
+    Serial.print("Roll: " + String(roll/10.0));
+    Serial.print(" Pitch: " + String(pitch/10.0));
+    Serial.println(" Yaw: " + String(yaw));
+}
