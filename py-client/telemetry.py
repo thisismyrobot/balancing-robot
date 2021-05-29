fe893a4763f4a2c87f04776f9fde880915148094
Robert <rwallhead@gmail.com>
2021-05-29 15:50:24 +1000

    Increased serial update rate and baud

--- py-client/telemetry.py
+++ py-client/telemetry.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 import multiwii
 
@@ -9,20 +10,21 @@ def stream(port, baud, duration=None):
     if duration is not None:
         duration = datetime.timedelta(seconds=duration)
 
-    board = multiwii.MultiWii(port)
+    board = multiwii.MultiWii(port, baud)
     while True:
         if start is None:
             start = datetime.datetime.now()
 
         actual_duration = datetime.datetime.now() - start
-        if duration is not None and actual_duration > duration:            
+        if duration is not None and actual_duration > duration:
             print(f'{results} results in {actual_duration.total_seconds()} = {(results / actual_duration.total_seconds())} Hz')
             break
 
-        print(board.getData(multiwii.MultiWii.ATTITUDE))
-        results += 1
-
+        data = board.getData(multiwii.MultiWii.ATTITUDE)
+        #print(data)
+        if data is not None:
+            results += 1
 
 
 if __name__ == '__main__':
-    stream('COM4', 115200)
+    stream('COM4', 38400, 10)
