8297b2d046305558bc2eb76df0647bd7000ea3d3
Robert <rwallhead@gmail.com>
2021-07-11 13:17:42 +1000

    working 333hz loop reading orientation

--- src/balancing-robot-controller/balancing-robot-controller.ino
+++ src/balancing-robot-controller/balancing-robot-controller.ino
@@ -1,12 +1,16 @@
 /*
  * Add https://dl.espressif.com/dl/package_esp32_index.json as an additional board manager URL.
- * Install esp32 board via the board manager.
+ * Install esp32 board via the board manager (1.0.6).
  * Select Heltec_Wifi_Kit_32 as board.
  * Install the MsgPack Library (0.3.8).
  * 
  */
 #include <Arduino.h>
+#include <HardwareSerial.h>
 #include "src/telemetry.h"
+
+// https://heltec-automation-docs.readthedocs.io/en/latest/esp32/frequently_asked_questions.html#vext-control
+#define F3_ENABLE_GPIO 21
 
 int watchdog = 0;
 int rate = 0;
@@ -23,9 +27,10 @@ TelemetryData_t telemetryData = {
     &pitch
 };
 
+HardwareSerial F3Serial(1);
+
 void setup() {
-    // F3 Board, using UART2 pins.
-    Serial2.begin(38400);
+    setupF3();
 
     startTelemetryTask((TelemetryData_t *)&telemetryData);
 }
@@ -34,6 +39,17 @@ void loop() {
     pitch = readPitch();
 
     updateStats();
+}
+
+void setupF3() {
+    pinMode(F3_ENABLE_GPIO, OUTPUT);
+    digitalWrite(F3_ENABLE_GPIO, HIGH);
+    delay(2000);
+    digitalWrite(F3_ENABLE_GPIO, LOW);
+    delay(5000);
+
+    // using UART2 pins.
+    F3Serial.begin(115200, SERIAL_8N1, 16, 17);  
 }
 
 void updateStats() {
@@ -58,14 +74,16 @@ double readPitch() {
     uint8_t n_bytes = 0;
     uint8_t checksum = 0;
 
-    Serial2.write((byte *) "$M<", 3);
-    Serial2.write(n_bytes);
+    F3Serial.write((byte *) "$M<", 3);
+    F3Serial.write(n_bytes);
     checksum ^= n_bytes;
 
-    Serial2.write(cmd);
+    F3Serial.write(cmd);
     checksum ^= cmd;
 
-    Serial2.write(checksum);
+    F3Serial.write(checksum);
+    F3Serial.flush(true);  // true = TX only.
+    delay(2);  // 2ms is stable too.
 
     // Read response    
     byte count = 0;
@@ -74,21 +92,18 @@ double readPitch() {
     int16_t pitch = 0;
     int16_t yaw = 0;
 
-    // Wait for anything.
-    while (!Serial2.available());
-
     // Descard everything until next '$'
-    while (Serial2.peek() != '$') {
-      Serial2.read();
+    while (F3Serial.peek() != '$') {
+      F3Serial.read();
     }
 
     // Full altitude message is 12 bytes.
-    while(Serial2.available() < 9);
+    while(F3Serial.available() < 9);
 
     // Read the rest.
-    while (Serial2.available()) {
+    while (F3Serial.available()) {
         count += 1;
-        byte c = Serial2.read();
+        byte c = F3Serial.read();
         switch (count) {
             case 6:
                 roll = c;
