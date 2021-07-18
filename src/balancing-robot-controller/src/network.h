e248a05d391dbf34f0991e8e90331adb58b478ca
Robert <rwallhead@gmail.com>
2021-07-18 14:18:12 +1000

    Missed header lead-in

--- src/balancing-robot-controller/src/network.h
+++ src/balancing-robot-controller/src/network.h
@@ -1,5 +1,10 @@
+#ifndef network_h
+#define network_h
+
 // This one's up to you :D
 const char* wifi_ssid = "";
 const char* wifi_pass = "";
 const char* target_ip = "";
 const int target_port = 9001;
+
+#endif
