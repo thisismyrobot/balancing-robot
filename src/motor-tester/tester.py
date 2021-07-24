806101087baf69781fb808ba56986ace04e842bb
Robert <rwallhead@gmail.com>
2021-07-24 10:15:39 +1000

    Basic calculator of motor performance

---
+++ src/motor-tester/tester.py
@@ -0,0 +1,61 @@
+"""
+A calculator of motor specs from video of the wheel lifting a weight.
+
+https://courses.lumenlearning.com/physics/chapter/7-3-gravitational-potential-energy/
+https://www.ftexploring.com/energy/power_1.html
+http://www.auburn.edu/academic/classes/scmh/1010/Energy%20and%20Work.php
+https://planetcalc.com/1908/
+
+"""
+import math
+
+
+# https://en.wikipedia.org/wiki/Gravity_of_Earth
+GRAVITY = 9.80665
+
+
+def question(text, default=None):
+    prompt = text + (f' ({default})' if default else '') + ': '
+    answer = input(prompt)
+    if answer == '':
+        if default:
+            answer = default
+        else:
+            answer = question(text, default)
+    return float(answer)
+
+
+def calculate(mass_kg, diameter_m, rotations_r, time_s):
+    # c = 2πr
+    height_m = math.pi * diameter_m
+
+    # W = mgh
+    work_J = mass_kg * GRAVITY * height_m
+
+    # P = W/t
+    power_W = work_J / time_s
+
+    print(f'{power_W} Watts of power')
+
+    # v = d / t
+    angular_velocity_rs = rotations_r / time_s
+
+    # τ = P / ω
+    torque_Nm = power_W / angular_velocity_rs
+
+    print(f'{torque_Nm} Nm of torque')
+
+    torque_kgcm = torque_Nm * (100 / GRAVITY);
+
+    print(f'{torque_kgcm} kg-cm of torque')
+
+
+
+if __name__ == '__main__':
+    mass_kg = question('Mass of weight in kilograms', 0.1)
+    diameter_m = question('Wheel diameter in metres', 0.0665)
+    rotations_r = question('Wheel rotations', 1)
+    ratio = question('Gearbox ratio (?:1)', 48)
+    time_s = question('Duration of lift in seconds')
+
+    calculate(mass_kg, diameter_m, rotations_r, time_s)
