86e2a685ad8a2ceeb60d47b9e1cb285f1dbaf20f
Robert <rwallhead@gmail.com>
2021-08-01 09:53:38 +1000

    Better listener output

--- src/telemetry-listener/listener.py
+++ src/telemetry-listener/listener.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import time
 
 import msgpack
@@ -7,41 +8,42 @@ import msgpack
 UDP_IP = "0.0.0.0"
 UDP_PORT = 9001
 
+# Uncomment to include in CSV output.
+PAYLOAD = {
+#    0: ('IP', '{}'),
+#    1: ('Packet', '{}'),
+#    2: ('Count', '{}'),
+#    3: ('Rate (Hz)', '{}'),
+#    4: ('Pitch Correct (°)', '{:.2f}'),
+#    5: ('Pitch (°)', '{:.2f}'),
+#    6: ('PID output (m/s)', '{:.3f}'),
+    7: ('Left Command (m/s)', '{:.3f}'),
+#    8: ('Right Command (m/s)', '{:.3f}'),
+    9: ('Left Actual (m/s)', '{:.3f}'),
+#    10: ('Right Actual (m/s)', '{:.3f}'),
+}
+
 
 def listen():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
 
+    print(','.join([p[0] for p in PAYLOAD.values()]))
+
     packets = 0
     while True:
         data, (ip, port) = sock.recvfrom(1024)
         message = msgpack.unpackb(data, use_list=False)
-        (
-            count,
-            rate,
-            pitch_correction,
-            pitch,
-            pid_pitch_output,
-            left_cmd,
-            right_cmd,
-            left_actual,
-            right_actual
-        ) = message
-        packets += 1
 
-        output = ','.join((
-            f'IP: {ip}',
-            f'Packet: {packets}',
-            f'Count: {count}',
-            f'Rate (Hz): {rate}',
-            f'Pitch Correct (°): {pitch_correction:.2f}',
-            f'Pitch (°): {pitch:.2f}',
-            f'PID output (m/s): {pid_pitch_output:.3f}',
-            f'Left (m/s): {left_cmd:.3f}, {left_actual:.3f}',
-            f'Right (m/s): {right_cmd:.3f}, {right_actual:.3f}',
-        ))
-        print(output)
+        packets += 1
+        payload = (ip, packets) + message
+
+        values = []
+        for index, (_, template) in PAYLOAD.items():
+            values.append(template.format(payload[index]))
+
+        print(','.join(values))
 
 
 if __name__ == '__main__':
