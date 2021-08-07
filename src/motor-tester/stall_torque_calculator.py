99328b8c76befa744042ab2565a1771a96859ae6
Robert <rwallhead@gmail.com>
2021-08-07 15:35:24 +1000

    Working stall measuring, but I think I need both directions

--- src/motor-tester/stall_torque_calculator.py
+++ src/motor-tester/stall_torque_calculator.py
@@ -11,8 +11,13 @@ https://planetcalc.com/1908/
 
 """
 import math
-import serial
+import msvcrt
+import statistics
+import sys
+import threading
 import time
+
+import serial
 
 
 # https://en.wikipedia.org/wiki/Gravity_of_Earth
@@ -40,34 +45,74 @@ def calculate(mass_kg, diameter_m, voltage_v):
 
     # Stall torque increases with voltage, fairly linearly, so we can
     # extrapolate a stall torque per volt value to compare motors by.
-    print(f'{torque_Nm:.2f}Nm ({torque_kgcm:.2f}kgf.cm) @ {voltage_v}V => {(torque_Nm/voltage_v):.3f}Nm/V ({(torque_kgcm/voltage_v):.2f}kgf.cm/V)')
+    print(f'{torque_Nm:.2f}Nm ({torque_kgcm:.2f}kgf.cm) @ {voltage_v:.2f}V => {(torque_Nm/voltage_v):.5f}Nm/V ({(torque_kgcm/voltage_v):.4f}kgf.cm/V)')
 
 
 def manual_entry():
     mass_kg = question('Mass of weight in kilograms', 0.05)
-    diameter_m = question('Wheel diameter in metres', 0.0665)
+    diameter_m = question('Wheel diameter in metres', 0.0479)
     voltage_v = question('Voltage when wheel moved')
 
     calculate(mass_kg, diameter_m, voltage_v)
 
 
-def measure_motor(port='COM1'):
+def measure_motor(port='COM8', samples=2, fallback=1.0):
     """Assumes Pololu qik 2s9v1 attached to COM1 and the motor to M0."""
-    speed = 0
+    mass_kg = question('Mass of weight in kilograms', 0.05)
+    diameter_m = question('Wheel diameter in metres', 0.0479)
+    supply_voltage_v = question('Supply voltage')
 
-    with serial.Serial('/dev/ttyS0', 38400, timeout=1) as conn:
+    global space_pressed
+    space_pressed = False
+    def space_listener_thread():
+        global space_pressed
+        while True:
+            ch = msvcrt.getch()
+            if ch == b' ':
+                space_pressed = True
+            else:
+                break
+            time.sleep(0.01)
+    threading.Thread(target=space_listener_thread).start()
+
+    move_points_v = []
+    motor_voltage_v = 0
+    with serial.Serial(port, 38400, timeout=1) as conn:
 
         # 8-bit PWM, 3.9 kHz.
         conn.write(bytearray([0x84, 0x01, 0x03, 0x55, 0x2A]))
 
         # Increase until move.
         while True:
-            print(speed)
-            if speed <= 127:
-                conn.write(bytearray([0x88, speed]))
+
+            if space_pressed:
+                space_pressed = False
+                print(f'{motor_voltage_v:.2f}V ({duty}/255)')
+
+                move_points_v.append(motor_voltage_v)
+                motor_voltage_v = max(0, motor_voltage_v - fallback)
+
+            duty = int((motor_voltage_v / supply_voltage_v) * 255.0)
+            duty = max(0, min(duty, 255))
+
+            if duty <= 127:
+                conn.write(bytearray([0x88, duty]))
             else:
-                conn.write(bytearray([0x89, speed - 128]))
+                conn.write(bytearray([0x89, duty - 128]))
+
+            if motor_voltage_v < supply_voltage_v:
+                motor_voltage_v += 0.01
+
             time.sleep(0.1)
+
+            if len(move_points_v) == samples:
+                conn.write(bytearray([0x86]))
+                break
+
+    average_move_v = statistics.mean(move_points_v)
+    points = ','.join([f'{p:.2f}' for p in move_points_v])
+    print(f'{average_move_v:.2f}V over {samples} samples: ({points})')
+    calculate(mass_kg, diameter_m, average_move_v)
 
 
 if __name__ == '__main__':
